74c71b119f6d19d9ad5bc6ae8e3b1fd7ab2c8f00
Davis Philip <115732383+mtrx28@users.noreply.github.com>
2024-11-05 22:38:08 +0530

    landing page

---
+++ client/src/pages/gymbro.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './gymbro.css';
+
+function Gymbro() {
+    return (
+        <div className="gymbroBody">
+
+
+            <div className="card">
+                {/* Card content can go here */}
+            </div>
+        </div>
+    );
+}
+
+export default Gymbro;
