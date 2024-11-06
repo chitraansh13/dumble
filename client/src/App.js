3d8543155e5bafa8c193902fff690e59907db9e6
Davis Philip <115732383+mtrx28@users.noreply.github.com>
2024-11-06 11:21:24 +0530

    landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Register from './pages/register';
-import Login from './pages/login';
 import './App.css';
-import Gymbro from './pages/gymbro';
+import Landing from './pages/landing';
 
 
 function App() {
     return (
-        <Router>
-            <div className="landingBody">
-
-                <img src="/ellipse.png" alt="Ellipse" className="ellipseImage" />
-
-
-                <img src="/landimg.png" alt="Running Woman" className="runningWomanImage" />
-
-
-                <div className="largeText">DUMBLE.</div>
-
-                <nav className="landingNav">
-                    <Link to="/">
-                        <img
-                            src="/logo192.png"
-                            alt="logo"
-                            className="App-logo"
-                            width={50}
-                            height={50}
-                        />
-                    </Link>
-
-                    <div>
-                        <Link to="/login" className="App-link">Login</Link>
-                        <Link to="/register" className="App-link">Register</Link>
-                        <Link to="/gymbro" className="App-link">Gymbro</Link>
-                    </div>
-                </nav>
-
-                <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/gymbro" element={<Gymbro />} /> {/* This is the new route */}
-                    <Route path="/" />
-                </Routes>
-
-            </div>
-        </Router>
+        <Landing />
     );
 }
 
