9ce1f9075fd2f42732f0c5ae3b0ce53dddd2e345
Chitraansh <91152476+Chitraansh@users.noreply.github.com>
2024-11-07 11:09:53 +0530

    fixed issues in prev commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing from './pages/landing';
-
+import Register from './pages/register';
+import Login from './pages/login';
+import Gymbro from './pages/gymbro';
 
 function App() {
     return (
-        <Landing />
+        <Router>
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/gymbro" element={<Gymbro />} />
+            </Routes>
+        </Router>
     );
 }
 
--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Register from './register';
-import Login from './login';
+import { Link } from 'react-router-dom';
 import './landing.css';
-import Gymbro from './gymbro';
-
 
 function Landing() {
-    return <Router>
+    return (
         <div className="landingBody">
-
             <img src="/ellipse.png" alt="Ellipse" className="ellipseImage" />
-
-
             <img src="/landimg.png" alt="Running Woman" className="runningWomanImage" />
-
-
             <div className="largeText">DUMBLE.</div>
-
             <nav className="landingNav">
                 <Link to="/">
                     <img
@@ -28,23 +18,14 @@ function Landing() {
                         height={50}
                     />
                 </Link>
-
                 <div>
                     <Link to="/login" className="App-link">Login</Link>
                     <Link to="/register" className="App-link">Register</Link>
                     <Link to="/gymbro" className="App-link">Gymbro</Link>
                 </div>
             </nav>
-
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/gymbro" element={<Gymbro />} /> {/* This is the new route */}
-                <Route path="/" />
-            </Routes>
-
         </div>
-    </Router>;
+    );
 }
 
 export default Landing;
