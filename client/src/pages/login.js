5796f0160a6bcfbbd7ee0d0ae4a42dc6779befd3
Chitraansh <91152476+Chitraansh@users.noreply.github.com>
2024-11-08 08:18:49 +0530

    idk

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './login.css';
 
-function About() {
-    return <h1>Login Page</h1>;
+function Login() {
+    return (
+        <div className="glass-bg">
+            <h1>Log in to your account.</h1>
+            <form method="POST">
+                <div className="text-input">
+                    <input type="text" placeholder="Email" name="email" />
+                    <br /><br />
+                    <input type="password" placeholder="Password" name="password" />
+                </div>
+                <div className="submit-buttons">
+                    <Link to="/gymbro" className='loginButton'>Login</Link>
+                    <a href="/register">New? Register here</a>
+                </div>
+            </form>
+        </div>
+    );
 }
 
-export default About;
+export default Login;
--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './register.css';
 
 function Home() {
-    return <h1>Register Page</h1>;
+    return ( 
+        <div className="glass-bg">
+            <h1>Register your account.</h1>
+            <form method="POST">
+                <div className="text-input">
+                    <input type="text" placeholder="Name" name="name" />
+                    <br /><br />
+                    <input type="text" placeholder="Email" name="email" />
+                    <br /><br />
+                    <input type="password" placeholder="Password" name="password" />
+                </div>
+                <div>
+                    <Link to="/gymbro" className='loginButton'>Register</Link>
+                    
+                </div>
+            </form>
+        </div>
+    );
 }
 
 export default Home;
